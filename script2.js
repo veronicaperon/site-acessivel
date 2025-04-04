0bbc175ed5317658ae204e5a232b4e23500c1980
veronicaperon <160857545+veronicaperon@users.noreply.github.com>
2025-04-04 07:32:11 -0300

    .

--- script2.js
+++ script2.js
@@ -1,1 +1,3 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
