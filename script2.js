38dce025049c5ccacbea0acc977eb305979358ed
veronicaperon <160857545+veronicaperon@users.noreply.github.com>
2025-04-04 07:40:06 -0300

    .

--- script2.js
+++ script2.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    })
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        let tamanhoAtualFonte = 1;
+    })
+    })
